41405b83ad9fad2d87e0905dc98a0a6bbbdc28a6
Ryan Fowler <ryan.fowler19@gmail.com>
2020-10-29 17:49:20 -0400

    Include version in log messages

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,7 +1,9 @@
+import { version } from "../package.json";
+
 import pino from "pino";
 
 export const logger = pino({
-  base: { service: "imaged" },
+  base: { service: "imaged", v: version },
   formatters: {
     level: (label) => {
       return { level: label };
