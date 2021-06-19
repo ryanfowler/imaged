6bc1081cbd9a89b5b1d2c4d5a49e3e5ec5bf303a
Ryan Fowler <ryan.fowler19@gmail.com>
2021-06-19 02:11:55 +0000

    Refactor integration tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,119 +1,51 @@
-import { URLSearchParams } from "url";
-
-import sharp from "sharp";
-import { request } from "undici";
-
-sharp.cache(false);
+import data from "./data.json";
+import { getImageMetadata } from "./utils";
 
 const timeout = 60_000;
 
-const images = [
-  {
-    name: "jpeg",
-    url: "https://user-images.githubusercontent.com/2668821/122620831-cec26080-d048-11eb-95a5-b6eaf986c13f.jpeg",
-  },
-];
-
-const imageTypes = [
-  {
-    name: "jpeg",
-    sharpFormat: "jpeg",
-  },
-  {
-    name: "webp",
-    sharpFormat: "webp",
-  },
-  {
-    name: "png",
-    sharpFormat: "png",
-  },
-  {
-    name: "avif",
-    sharpFormat: "heif",
-  },
-  {
-    name: "tiff",
-    sharpFormat: "tiff",
-  },
-];
-
-images.forEach((value) => {
-  imageTypes.forEach(async (imageType) => {
+data.inputs.forEach((input) => {
+  data.outputs.forEach(async (output) => {
     test.concurrent(
-      `input ${value.name} to output ${imageType.name}`,
+      `crop height - input ${input.format} to output ${output.format}`,
       async () => {
-        const metadata = await getImageMetadata(value.url, {
-          format: encodeURIComponent(imageType.name),
-          height: "200",
+        const metadata = await getImageMetadata(input.url, {
+          format: output.format,
+          height: "100",
         });
-        expect(metadata.format).toEqual(imageType.sharpFormat);
+        expect(metadata.format).toEqual(output.sharpFormat);
+        expect(metadata.height).toEqual(100);
+        expect(metadata.width).toEqual(133);
+      },
+      timeout
+    );
+
+    test.concurrent(
+      `crop width - input ${input.format} to output ${output.format}`,
+      async () => {
+        const metadata = await getImageMetadata(input.url, {
+          format: output.format,
+          width: "133",
+        });
+        expect(metadata.format).toEqual(output.sharpFormat);
+        expect(metadata.height).toEqual(100);
+        expect(metadata.width).toEqual(133);
+      },
+      timeout
+    );
+
+    test.concurrent(
+      `crop height & width - input ${input.format} to output ${output.format}`,
+      async () => {
+        const metadata = await getImageMetadata(input.url, {
+          format: output.format,
+          height: "100",
+          width: "100",
+        });
+        expect(metadata.format).toEqual(output.sharpFormat);
+        expect(metadata.height).toEqual(100);
+        expect(metadata.width).toEqual(100);
       },
       timeout
     );
   });
 });
-
-test.concurrent(
-  `crop to specific height`,
-  async () => {
-    const metadata = await getImageMetadata(images[0].url, {
-      height: "400",
-    });
-    expect(metadata.height).toEqual(400);
-    expect(metadata.width).toEqual(533);
-  },
-  timeout
-);
-
-test.concurrent(
-  `crop to specific width`,
-  async () => {
-    const metadata = await getImageMetadata(images[0].url, {
-      width: "533",
-    });
-    expect(metadata.height).toEqual(400);
-    expect(metadata.width).toEqual(533);
-  },
-  timeout
-);
-
-test.concurrent(
-  `crop to specific height and width`,
-  async () => {
-    const metadata = await getImageMetadata(images[0].url, {
-      height: "400",
-      width: "400",
-    });
-    expect(metadata.height).toEqual(400);
-    expect(metadata.width).toEqual(400);
-  },
-  timeout
-);
-
-async function getImageMetadata(
-  imageUrl: string,
-  options?: Record<string, string>
-): Promise<sharp.Metadata> {
-  let urlStr = `http://localhost:9000/proxy/${encodeURIComponent(imageUrl)}`;
-  if (options) {
-    const query = new URLSearchParams();
-    for (const [key, value] of Object.entries(options)) {
-      query.set(key, value);
-    }
-    urlStr += `?${query.toString()}`;
-  }
-  const out = await fetch(urlStr);
-  return sharp(out, { sequentialRead: true }).metadata();
-}
-
-async function fetch(url: string): Promise<Buffer> {
-  const res = await request(url);
-  if (res.statusCode !== 200) {
-    throw new Error(`fetch: received response code '${res.statusCode}'`);
-  }
-  const out = [];
-  for await (const data of res.body) {
-    out.push(data);
-  }
-  return Buffer.concat(out);
-}
